9b0452853d3a341aba26bb7d8310c0e88615044c
Carlos Ernesto Vazquez <cvazquez@entravision.com>
2018-12-29 19:29:50 -0800

    Well commited

---
+++ keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://Airestereo:Lunabella14@airestereo-shard-00-00-brxfy.mongodb.net:27017,airestereo-shard-00-01-brxfy.mongodb.net:27017,airestereo-shard-00-02-brxfy.mongodb.net:27017/test?ssl=true&replicaSet=Airestereo-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "SECRET"
+};
